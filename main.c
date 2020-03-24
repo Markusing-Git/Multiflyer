25f231a5a5d7a204fcf4b23b788a73e6cf70bd79
markus ingemarsson <markuscontacting@gmail.com>
2020-03-24 20:46:35 +0100

    omgjord main

--- main.c
+++ main.c
@@ -2,29 +2,37 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_net.h>
-#include <SDL_timer.h>
+#include <stdbool.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
 
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_INIT_VIDEO);
+
+	bool quit = false;
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
 
 	SDL_Window* gWindow = SDL_CreateWindow("SDL Example",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
 		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-	SDL_Surface* gScreenSurface = SDL_GetWindowSurface(gWindow);
-	SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,
-	SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
 
-	SDL_FillRect(gScreenSurface, &fillRect, 0x3AD32F);
 
 	SDL_UpdateWindowSurface(gWindow);
 
-	SDL_Delay(10000);
+	while (!quit) {
+		//här lägger vi alla events
+
+	}
 
 	SDL_DestroyWindow(gWindow);
+
+	SDL_Quit();
+
 	return 0;
 }
