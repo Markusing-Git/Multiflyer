d0974f657355cf90142971922eceb77e85dc153c
Joel Özmen <JoelOzmen@Elevs-MacBook-Air.local>
2020-03-27 14:06:54 +0100

    Hej

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ SDL_Window* window = NULL;
 int main(void)
 {
     // Initialize SDL
+    //Joel
     bool running = true;
     if (SDL_Init(SDL_INIT_VIDEO != 0)) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
