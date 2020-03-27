7bbb3bb9d04b5d0dd18f9db3375745ab7fdd600a
markus ingemarsson <markuscontacting@gmail.com>
2020-03-27 13:54:51 +0100

    Fix för alla

--- main.c
+++ main.c
@@ -1,68 +1,39 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
+#define width 640
+#define height 480
 
-bool init();
-void close();
+SDL_Window* window = NULL;
+int main(void)
+{
+    // Initialize SDL
+    bool running = true;
+    if (SDL_Init(SDL_INIT_VIDEO != 0)) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        running = false;
+    }
+    else
+    {
+        // Create a SDL window
+        window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+        bool running = true;
+        SDL_Event event;
+        while (running)
+        {
+            // Process events
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_QUIT)
+                {
+                    running = false;
+                }
+            }
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-
-int main(int argc, char* args[]) {
-	
-	bool running = false;
-	
-	if (!init()) {
-		printf("Failed to initialize!\n");
-	}
-
-
-	while (!running) {
-		// här anropas allt när spelet körs
-		SDL_Delay(5000);
-		running = true;
-	}
-
-	SDL_UpdateWindowSurface(gWindow);
-	close();
-
-	return 0;
-}
-
-bool init(void) {
-	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO != 0)) {
-		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-		success = false;
-	}
-	else{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Example",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL){
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface(gWindow);
-		}
-	}
-	return success;
-}
-
-void close(void){
-	//Destroy window
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-
-	//Quit SDL subsystems
-	SDL_Quit();
+    return 0;
 }
