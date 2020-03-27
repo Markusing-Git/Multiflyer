be19bdcf651eec137d9c73197e1d9923601dac76
Hoang Pham <ehuypha@Hoangs-MacBook-Pro.local>
2020-03-27 13:22:02 +0100

    Test igen

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #define width 640
@@ -11,55 +10,6 @@ int main(void)
     // Initialize SDL
     bool running = true;
     if (SDL_Init(SDL_INIT_VIDEO != 0)) {
-=======
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
-#include <stdbool.h>
-
-bool init();
-void close();
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-
-int main(int argc, char* args[]) {
-	
-	bool running = false;
-	
-	if (!init()) {
-		printf("Failed to initialize!\n");
-		printf("Hej");
-		//Hoang
-		//jacob
-		//Test
-		//Joel
-	}
-
-
-	while (!running) {
-		//H�r l�gger vi allt som anropas n�r spelet k�rs
-		SDL_Delay(10000);
-		running = true;
-		
-		
-	}
-	
-	SDL_UpdateWindowSurface(gWindow);
-	close();
-
-	return 0;
-}
-
-bool init(void) {
-	bool success = true;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO != 0)) {
->>>>>>> ec2a5d57218cc061111222bbc151e61a75585df3
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		running = false;
     }
