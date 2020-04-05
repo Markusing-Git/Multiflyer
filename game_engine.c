d73286cde8cad650cbe7f5a4f5355051df96d4b8
Jonatan Larsson <jonatan.jonte.larsson@gmail.com>
2020-04-05 15:16:19 +0200

    Kod för bakgrund och sprite samt spritesheet bilden och en till dll fil

--- game_engine.c
+++ game_engine.c
@@ -1,11 +1,31 @@
 #include "game_engine.h"
 
+#define nrOfFrames 3 //Antal frames i spritesheet
+
+bool loadMedia(SDL_Renderer* renderer);
+
+static SDL_Texture* flyTex = NULL;
+static SDL_Surface* flySurface = NULL;
+static SDL_Rect spriteClips[nrOfFrames];
+
 
 bool startGame(SDL_Renderer* renderer, int w, int h) {
+
+    int frame = 0; //Den frame som ska visas
+    SDL_Rect currentClip; //Tillfällig rect för att visa sprite:n. Ska ersättas med player
+    currentClip.x = 50;
+    currentClip.y = 50;
+    currentClip.w = 64;
+    currentClip.h = 205;
 
     //Create Envoirment
     bool running = true;
     SDL_Event event;
+
+    //Laddar in spritesheet och skapar de olika frames:en för spritesheetet
+    if (!loadMedia(renderer)) {
+        running = false; //Spelet stängs ner ifall det skett ett error i loadMedia
+    }
 
     //Starting game engine
     while (running)
@@ -21,13 +41,54 @@ bool startGame(SDL_Renderer* renderer, int w, int h) {
 
         //updating positions,inputs,multiplayer sends and receives.
 
+        //Uppdaterar frames:en, kodblocket skapar en liten delay i bytet mellan frames:en
+        frame++;
+        if (frame / 3 == nrOfFrames) {
+            frame = 0;
+        }
 
         // clear the window and render updates
         SDL_RenderClear(renderer);
+        SDL_RenderCopyEx(renderer, flyTex, &spriteClips[frame / 3], &currentClip, 0, NULL, SDL_FLIP_NONE); //Visar spriten
         SDL_RenderPresent(renderer);
 
         // 60 FPS
         SDL_Delay(1000 / 60); 
     }
     return true;
+}
+
+bool loadMedia(SDL_Renderer* renderer) {
+    bool noError = true;
+    flySurface = IMG_Load("flySpriteSheet.png"); //Laddar in spritesheet
+    if (flySurface == NULL) {
+        printf("Unable to load image. Error: %s", SDL_GetError());  //Kollar efter error vid IMG_Load
+        noError = false;
+    }
+    else {
+        flyTex = SDL_CreateTextureFromSurface(renderer, flySurface); //skapar en texture från spritesheet
+        if (flyTex == NULL)
+        {
+            flyTex = SDL_CreateTextureFromSurface(renderer, flySurface); //skapar en texture från spritesheet 
+            printf("Unable to create texture from surface. Error: %s", SDL_GetError()); //Kollar efter error vid SDL_CreateTextureFromSurface
+            noError = false;
+        }
+        else {
+            spriteClips[0].x = 0;   //Skapar de olika rect för frames:en i spritesheet, vår spritesheet har 3 frames därav 3 rects
+            spriteClips[0].y = 0;
+            spriteClips[0].w = 117;
+            spriteClips[0].h = 150;
+
+            spriteClips[1].x = 117;
+            spriteClips[1].y = 0;
+            spriteClips[1].w = 117;
+            spriteClips[1].h = 150;
+
+            spriteClips[2].x = 234;
+            spriteClips[2].y = 0;
+            spriteClips[2].w = 117;
+            spriteClips[2].h = 150;
+        }
+    }
+    return noError; //Returnerar errorchecken
 }
