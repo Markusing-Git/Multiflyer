44e6d9a4f598fd5cbfd7a1ffea406a12924eeed3
Jacob Henningsson <jacob.henningsson@hotmail.com>
2020-05-05 12:59:14 +0200

    Tagit bort nätverks funktioner från game engine

--- game_engine.c
+++ game_engine.c
@@ -28,9 +28,8 @@ bool startGame(SDL_Renderer* renderer, int w, int h, char playerName[], char pla
 
     //Starting network
 
-    clientConnection(setup, playerIp, playerName, 0); //Sätt sync till 1 för att aktivera nätverks sync. Host måste startas innan klient   
-    create_Game_state(players, current, current->nrOfPlayers);
-    int_client_network(playerIp, setup, 2000, 2001);
+    start_Game_state(players, current);
+    intServerNetwork(current,setup);
 
     //***************************************************  STARTING GAME ENGINE  *****************************************************
     while (running)
@@ -43,12 +42,8 @@ bool startGame(SDL_Renderer* renderer, int w, int h, char playerName[], char pla
 
         uppdateInputs(players[0], input);
 
-        SetPlayerAlive(current, getPlayerStatus(players[0]), 0);
-
         if (current->nrOfPlayers > 1)
-            sendAndRecive(current, setup, playerPos[0], playerPos[1]);
-
-        setPlayerStatus(players[1], current->player_Alive[1]);
+            sendAndRecive(current, setup, playerPos,players);
 
         worldCollision(getPlayerPosAdr(players[0]), players[0], w, h);
 
